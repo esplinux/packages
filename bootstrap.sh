ef26f8c28cdab28552322932a859515cb9c2ba0a
Eric Molitor <eric@molitor.org>
2020-03-22 15:04:45 +0000

    Update with curl, gettext-tiny less netbsd-curses python and zlib

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,35 @@
 #!/bin/sh -eu
 
-if ! type "samu" > /dev/null; then
-  SAMU='ninja'
-else
-  SAMU='samu'
-fi
+export CC=clang
+export CXX=clang++
 
-$SAMU -C musl "$@"
-LDFLAGS=-static "$SAMU" -C dash "$@"
-LDFLAGS=-static "$SAMU" -C toybox "$@"
-LDFLAGS=-static "$SAMU" -C make "$@"
-LDFLAGS=-static "$SAMU" -C byacc "$@"
+export CFLAGS='-Oz -march=broadwell -fno-asynchronous-unwind-tables -fno-strict-aliasing'
+export CXXFLAGS='-Oz -march=broadwell'
+export LDFLAGS='-fuse-ld=lld -w -s'
+
+# Bootstrap Libraries
+samu -C musl "$@"
+samu -C zlib "$@"
+samu -C netbsd-curses "$@"
+
+samu -C toybox "$@"
+samu -C dash "$@"
+samu -C awk "$@"
+samu -C byacc "$@"
+samu -C gettext-tiny "$@"
+samu -C samurai "$@"
+samu -C make "$@"
+samu -C python "$@"
+
+
+#These have dependencies other than libc
+########################################
+
+# Cleanly depends upon bearssl and libz just manually deploy those
+samu -C curl "$@"
+
+# We use a different version of curses so statically link
+LDFLAGS="$LDFLAGS -static" samu -C less "$@"
+
+# C++ so statically link
+LDFLAGS="$LDFLAGS -static" samu -C cmake "$@"
