34092e3a42c0f0db52542062a3c4646d2dd28331
Eric Molitor <eric@molitor.org>
2020-03-31 13:28:51 +0100

    Update generate to use posix macros

--- generate.sh
+++ generate.sh
@@ -18,84 +18,88 @@ error() {
   exit 1
 }
 
-input="$1"
-while IFS=':' read -r lhs rhs
-do
-  KEY=$(printf %s "$lhs" | awk '{$1=$1};1')
-  VALUE=$(printf %s "$rhs" | awk '{$1=$1};1')
-
-  case $KEY in
-    "#"*) ;; # Skip comments
-    '') ;; # Skip blank lines
-    'PACKAGE') PACKAGE=$VALUE ;;
-    'VERSION') VERSION=$VALUE ;;
-    'PATCH') PATCH=$VALUE ;;
-    'URL') URL=$VALUE ;;
-    'GIT') GIT=$VALUE ;;
-    'TEMPLATE') TEMPLATE=$VALUE ;;
-    'TARGET') TARGET=$VALUE ;;
-    'INSTALL_TARGET') INSTALL_TARGET=$VALUE ;;
-    'AUTOMAKE_OPTIONS') AUTOMAKE_OPTIONS=$VALUE ;;
-    'CMAKE_OPTIONS') CMAKE_OPTIONS=$VALUE ;;
-    *) error "Unkown key $KEY" ;;
-  esac
-done < "$input"
-
-[ -z "$PACKAGE" ] && error "PACKAGE missing"
-[ -z "$VERSION" ] && error "VERSION missing"
-
-{ 
-  printf '.POSIX:\n';
-  printf 'PACKAGE=%s\n' "$PACKAGE";
-  printf 'VERSION=%s\n' "$VERSION";
-  printf '\n';
-
-  printf 'all: ${PACKAGE}-${VERSION}.tar.gz\n';
-  printf '\n';
-
-  if [ -n "$URL" ]
-  then
-    FILENAME="${URL##*/}";
-    printf 'src:\n';
-    printf '\tcurl -sSLO %s\n' "$URL";
-    printf '\ttar xf %s\n' $FILENAME;
-    printf '\trm %s\n' $FILENAME;
-    printf '\tmv ${PACKAGE}-${VERSION} src\n';
-    [ -z "$PATCH" ] || printf '\tpatch -p1 -i %s\n' "$PATCH";
+generate() {
+  input="$1"
+  while IFS='=' read -r lhs rhs
+  do
+    KEY=$(printf %s "$lhs" | awk '{$1=$1};1')
+    VALUE=$(printf %s "$rhs" | awk '{$1=$1};1')
+  
+    case $KEY in
+      "#"*) ;; # Skip comments
+      '') ;; # Skip blank lines
+      'PACKAGE') PACKAGE=$VALUE ;;
+      'VERSION') VERSION=$VALUE ;;
+      'PATCH') PATCH=$VALUE ;;
+      'URL') URL=$VALUE ;;
+      'GIT') GIT=$VALUE ;;
+      'TEMPLATE') TEMPLATE=$VALUE ;;
+      'TARGET') TARGET=$VALUE ;;
+      'INSTALL_TARGET') INSTALL_TARGET=$VALUE ;;
+      'AUTOMAKE_OPTIONS') AUTOMAKE_OPTIONS=$VALUE ;;
+      'CMAKE_OPTIONS') CMAKE_OPTIONS=$VALUE ;;
+      *) error "Unkown key $KEY" ;;
+    esac
+  done < "$input"
+  
+  [ -z "$PACKAGE" ] && error "PACKAGE missing"
+  [ -z "$VERSION" ] && error "VERSION missing"
+  
+  { 
+    printf '.POSIX:\n';
+    printf 'PACKAGE=%s\n' "$PACKAGE";
+    printf 'VERSION=%s\n' "$VERSION";
     printf '\n';
-  fi
-
-  if [ -n "$GIT" ]
-  then
-    printf 'src:\n';
-    printf '\tgit clone %s src\n' "$GIT";
-    [ -z "$PATCH" ] || printf '\tpatch -p1 -i %s\n' "$PATCH";
+  
+    printf 'all: $(PACKAGE)-$(VERSION).tar.gz\n';
     printf '\n';
-  fi
-
-  case $TEMPLATE in
-    "cmake")
-      printf "out: src\n";
-      printf "\tcmake -S src -B build $CMAKE_OPTIONS\n";
-      printf "\tmake -C build $TARGET\n";
-      printf "\tmake -C build $INSTALL_TARGET\n";
+  
+    if [ -n "$URL" ]
+    then
+      FILENAME="${URL##*/}";
+      printf 'src:\n';
+      printf '\tcurl -sSLO %s\n' "$URL";
+      printf '\ttar xf %s\n' $FILENAME;
+      printf '\trm %s\n' $FILENAME;
+      printf '\tmv $(PACKAGE)-$(VERSION) src\n';
+      [ -z "$PATCH" ] || printf '\tpatch -p1 -i %s\n' "$PATCH";
       printf '\n';
-    ;;
-    "automake")
-      printf "out: src\n";
-      printf "\tcd src; ./configure $AUTOMAKE_OPTIONS\n";
-      printf "\tmake -C src $TARGET\n";
-      printf "\tmake -C src $INSTALL_TARGET\n";
+    fi
+  
+    if [ -n "$GIT" ]
+    then
+      printf 'src:\n';
+      printf '\tgit clone %s src\n' "$GIT";
+      [ -z "$PATCH" ] || printf '\tpatch -p1 -i %s\n' "$PATCH";
       printf '\n';
-    ;;
-  esac
+    fi
+  
+    case $TEMPLATE in
+      "cmake")
+        printf "out: src\n";
+        printf "\tcmake -S src -B build $CMAKE_OPTIONS\n";
+        printf "\tmake -C build $TARGET\n";
+        printf "\tmake -C build $INSTALL_TARGET\n";
+        printf '\n';
+      ;;
+      "automake")
+        printf "out: src\n";
+        printf "\tcd src; ./configure $AUTOMAKE_OPTIONS\n";
+        printf "\tmake -C src $TARGET\n";
+        printf "\tmake -C src $INSTALL_TARGET\n";
+        printf '\n';
+      ;;
+    esac
+  
+    printf '${PACKAGE}-${VERSION}.tar.gz: out\n';
+    printf '\tcd out; tar -czf ../$(PACKAGE)-$(VERSION).tgz .\n';
+    printf '\n';
+    printf 'clean:\n';
+    printf '\trm -rf build out $(PACKAGE)-$(VERSION).tgz\n';
+    printf '\n';
+    printf 'distclean: clean\n';
+    printf '\trm -rf src\n';
+  } > Makefile
+}
 
-  printf '${PACKAGE}-${VERSION}.tar.gz: out\n';
-  printf '\tcd out; tar -czf ../${PACKAGE}-${VERSION}.tar.gz .\n';
-  printf '\n';
-  printf 'clean:\n';
-  printf '\trm -rf build out\n';
-  printf '\n';
-  printf 'distclean: clean\n';
-  printf '\trm -rf src\n';
-} > Makefile
+generate $1
