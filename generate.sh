099250e7a4465fd4b9804f4adf5e58f7af8755aa
Eric Molitor <eric@molitor.org>
2020-04-03 09:22:14 +0100

    Start to add parallel make support

--- generate.sh
+++ generate.sh
@@ -1,6 +1,7 @@
 #!/bin/sh -eu
 
 PACKAGES=""
+CURDIR=$(pwd)
 
 error() {
   printf "ERROR: %s\n" "$1"
@@ -9,24 +10,29 @@ error() {
 
 substitute() {
   RESULT=$1
-  RESULT=$( printf '%s' "$RESULT" | sed s/\$PACKAGE/"$PACKAGE"/ )
-  RESULT=$( printf '%s' "$RESULT" | sed s/\$VERSION/"$VERSION"/ )
+  RESULT=$( printf '%s' "$RESULT" | sed s?\$PACKAGE?"$PACKAGE"? )
+  RESULT=$( printf '%s' "$RESULT" | sed s?\$VERSION?"$VERSION"? )
+  RESULT=$( printf '%s' "$RESULT" | sed s?\$OUT?"$OUT"? )
   printf '%s' "$RESULT"
 }
 
 generate() {
-  INPUT="$1"
+  INPUT=$(realpath "$1")
+  BASE=$(dirname "$INPUT")
 
-  PACKAGE=""
-  VERSION=""
-  PATCH=""
-  URL=""
-  GIT=""
-  TEMPLATE="automake"
-  TARGET=""
-  INSTALL_TARGET="DESTDIR=$PWD/out install"
+  PACKAGE=''
+  VERSION=''
+  PATCH=''
+  URL=''
+  GIT=''
+  SRC='$PACKAGE-$VERSION'
+  BUILD='build'
+  OUT='out'
+  TEMPLATE='automake'
+  TARGET=''
+  INSTALL_TARGET='DESTDIR=$OUT install'
   AUTOMAKE_OPTIONS="--prefix=''"
-  CMAKE_OPTIONS=""
+  CMAKE_OPTIONS=''
 
   while IFS='=' read -r lhs rhs
   do
@@ -38,9 +44,12 @@ generate() {
       '') ;; # Skip blank lines
       'PACKAGE') PACKAGE=$VALUE ;;
       'VERSION') VERSION=$VALUE ;;
+      'OUT') OUT=$VALUE ;;
       'PATCH') PATCH=$VALUE ;;
       'URL') URL=$VALUE ;;
       'GIT') GIT=$VALUE ;;
+      'SRC') SRC=$VALUE ;;
+      'BUILD') BUILD=$VALUE ;;
       'TEMPLATE') TEMPLATE=$VALUE ;;
       'TARGET') TARGET=$VALUE ;;
       'INSTALL_TARGET') INSTALL_TARGET=$VALUE ;;
@@ -57,9 +66,17 @@ generate() {
   PACKAGES="$PACKAGE $PACKAGES"
 
   # These values are interpolated
+  OUT=$( substitute "$OUT" )
+  OUT="$BASE/$OUT"
+  SRC=$( substitute "$SRC" )
+  SRC="$BASE/$SRC"
+  BUILD=$( substitute "$BUILD" )
+  BUILD="$BASE/$BUILD"
   PATCH=$( substitute "$PATCH" )
   URL=$( substitute "$URL" )
   GIT=$( substitute "$GIT" )
+  TARGET=$( substitute "$TARGET" )
+  INSTALL_TARGET=$( substitute "$INSTALL_TARGET" )
 
   { 
     printf '%s: %s-%s.tar.gz\n' "$PACKAGE" "$PACKAGE" "$VERSION";
@@ -67,49 +84,45 @@ generate() {
   
     if [ -n "$URL" ]
     then
-      FILENAME="${URL##*/}";
-      printf 'src:\n';
-      printf '\tcurl -sSLO %s\n' "$URL";
-      printf '\ttar xf %s\n' "$FILENAME";
-      printf '\trm %s\n' "$FILENAME";
-      printf '\tmv %s-%s src\n' "$PACKAGE" "$VERSION";
+      printf '%s:\n' "$SRC";
+      printf '\tcurl -sSL %s | tar -xz\n' "$URL";
       [ -z "$PATCH" ] || printf '\tpatch -p1 -i %s\n' "$PATCH";
       printf '\n';
     fi
   
     if [ -n "$GIT" ]
     then
-      printf 'src:\n';
-      printf '\tgit clone %s src\n' "$GIT";
+      printf '%s:\n' "$SRC";
+      printf '\tgit clone %s %s\n' "$GIT" "$SRC";
       [ -z "$PATCH" ] || printf '\tpatch -p1 -i %s\n' "$PATCH";
       printf '\n';
     fi
   
     case $TEMPLATE in
       "cmake")
-        printf "out: src\n";
-        printf "\tcmake -S src -B build %s\n" "$CMAKE_OPTIONS";
-        printf "\tmake -C build %s\n" "$TARGET";
-        printf "\tmake -C build %s\n" "$INSTALL_TARGET";
+        printf '%s: %s\n' "$OUT" "$SRC";
+        printf '\tcmake -S %s -B %s %s\n' "$SRC" "$BUILD" "$CMAKE_OPTIONS";
+	printf '\t$(MAKE) -C %s %s\n' "$BUILD" "$TARGET";
+	printf '\t$(MAKE) -C %s %s\n' "$BUILD" "$INSTALL_TARGET";
         printf '\n';
       ;;
       "automake")
-        printf "out: src\n";
-        printf "\tcd src; ./configure %s\n" "$AUTOMAKE_OPTIONS";
-        printf "\tmake -C src %s\n" "$TARGET";
-        printf "\tmake -C src %s\n" "$INSTALL_TARGET";
+        printf '%s: %s\n' "$OUT" "$SRC";
+        printf '\tcd %s; ./configure %s\n' "$SRC" "$AUTOMAKE_OPTIONS";
+	printf '\tcd %s; $(MAKE) %s\n' "$SRC" "$TARGET";
+	printf '\tcd %s; $(MAKE) %s\n' "$SRC" "$INSTALL_TARGET";
         printf '\n';
       ;;
     esac
   
-    printf '%s-%s.tar.gz: out\n' "$PACKAGE" "$VERSION";
-    printf '\tcd out; tar -czf ../%s-%s.tgz .\n' "$PACKAGE" "$VERSION";
+    printf '%s-%s.tar.gz: %s\n' "$PACKAGE" "$VERSION" "$OUT";
+    printf '\tcd %s; tar -czf ../%s-%s.tgz .\n' "$OUT" "$PACKAGE" "$VERSION";
     printf '\n';
     printf 'clean:\n';
-    printf '\trm -rf build out %s-%s.tgz\n' "$PACKAGE" "$VERSION";
+    printf '\trm -rf %s %s %s-%s.tgz\n' "$BUILD" "$OUT" "$PACKAGE" "$VERSION";
     printf '\n';
     printf 'distclean: clean\n';
-    printf '\trm -rf src\n';
+    printf '\trm -rf %s\n' "$SRC";
     printf '\n';
   } >> .Makefile
 }
