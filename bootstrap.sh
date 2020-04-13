f528cb00a07e21e689a02e1044c5cf206cc8a048
Eric Molitor <eric@molitor.org>
2020-04-13 11:45:59 +0100

    Cleanup bootstrap builds

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,14 +6,14 @@ espbuild -D BOOTSTRAP linuxHeaders.esp cacert.esp musl.esp curses.esp zlib.esp b
 
 mkdir sysroot
 
-tar -C sysroot -xf musl-*.tgz
+#tar -C sysroot -xf musl-*.tgz
 tar -C sysroot -xf cacert-*.tgz
 tar -C sysroot -xf toybox-*.tgz
 tar -C sysroot -xf awk-*.tgz
-tar -C sysroot -xf byacc-*.tgz
+#tar -C sysroot -xf byacc-*.tgz
 tar -C sysroot -xf dash-*.tgz
-tar -C sysroot -xf less-*.tgz
-tar -C sysroot -xf make-*.tgz
+#tar -C sysroot -xf less-*.tgz
+#tar -C sysroot -xf make-*.tgz
 tar -C sysroot -xf curl-*.tgz
 
 cd sysroot
