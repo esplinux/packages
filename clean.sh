244f0b8965984a8683b3d30562851f9e803d906f
Eric Molitor <eric@molitor.org>
2020-04-14 13:25:41 +0100

    Add lld.esp, clang.esp and llvm.esp

--- clean.sh
+++ clean.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -eu
 
-rm -rf linux-* musl-* dash-* awk-* byacc-* toybox-* cacert-* netbsd-curses-* less-* zlib-* make-* curl-* BearSSL-* llvm-* sysroot *.tgz
+rm -rf linux-* musl-* dash-* awk-* byacc-* toybox-* cacert-* netbsd-curses-* less-* zlib-* make-* curl-* BearSSL-* llvm-* lld-* clang-* sysroot *.tgz
