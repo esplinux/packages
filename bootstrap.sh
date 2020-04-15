df074cb5555414461b585cb604955561e337d051
Eric Molitor <eric@molitor.org>
2020-04-15 15:17:17 +0100

    Update bootstrap to include clang, lld and libcxx

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,19 +2,22 @@
 
 ./clean.sh
 
-espbuild -D BOOTSTRAP linuxHeaders.esp cacert.esp musl.esp curses.esp zlib.esp bearssl.esp dash.esp awk.esp byacc.esp less.esp make.esp curl.esp toybox.esp
+espbuild -D BOOTSTRAP linuxHeaders.esp cacert.esp musl.esp dash.esp awk.esp make.esp curl.esp clang.esp lld.esp libcxx.esp toybox.esp
 
 mkdir sysroot
 
-#tar -C sysroot -xf musl-*.tgz
+tar -C sysroot -xf musl-*.tgz
 tar -C sysroot -xf cacert-*.tgz
 tar -C sysroot -xf toybox-*.tgz
 tar -C sysroot -xf awk-*.tgz
 #tar -C sysroot -xf byacc-*.tgz
 tar -C sysroot -xf dash-*.tgz
 #tar -C sysroot -xf less-*.tgz
-#tar -C sysroot -xf make-*.tgz
+tar -C sysroot -xf make-*.tgz
 tar -C sysroot -xf curl-*.tgz
+tar -C sysroot -xf clang-*.tgz
+tar -C sysroot -xf lld-*.tgz
+tar -C sysroot -xf libcxx-*.tgz
 
 cd sysroot
 tar -czf ../sysroot.tgz *
