d7b7ffb617f66c4618d169aa696c1231f83b0efc
Eric Molitor <eric@molitor.org>
2020-04-19 15:39:24 +0100

    Add host.esp and use it to populate tooling

--- clean.sh
+++ clean.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -eu
 
-rm -rf linux-* musl-* dash-* awk-* byacc-* toybox-* cacert-* netbsd-curses-* less-* zlib-* make-* curl-* BearSSL-* llvm-* lld-* clang-* libcxx-* sysroot *.tgz
+rm -rf linux-* musl-* dash-* awk-* byacc-* toybox-* cacert-* netbsd-curses-* less-* zlib-* libxml2-* make-* curl-* BearSSL-* llvm-* lld-* clang-* libcxx-* Python-* sysroot *.tgz
