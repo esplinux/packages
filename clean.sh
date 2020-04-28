a7246b4319f1bebc258375aa15fb118802658519
Eric Molitor <eric@molitor.org>
2020-04-28 18:57:32 +0100

    Add test.dist and skeleton

--- clean.sh
+++ clean.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -eu
 
-rm -rf linux-* musl-* dash-* awk-* byacc-* toybox-* cacert-* netbsd-curses-* less-* zlib-* libxml2-* make-* curl-* BearSSL-* llvm-* lld-* clang-* libcxx-* Python-* cmake-* compiler-rt-* libunwind-* libcxxabi-* libcxx-* libaio-* espbuild-* LVM2.* linux-* sysroot sinit-* *.tgz
+rm -rf linux-* musl-* dash-* awk-* byacc-* toybox-* cacert-* netbsd-curses-* less-* zlib-* libxml2-* make-* curl-* BearSSL-* llvm-* lld-* clang-* libcxx-* Python-* cmake-* compiler-rt-* libunwind-* libcxxabi-* libcxx-* libaio-* espbuild-* LVM2.* linux-* sinit-* *.tgz sysroot initramfs
