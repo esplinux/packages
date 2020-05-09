bc1c0f1b36d0cab6432e8d658544d6c71e3c1d91
Eric Molitor <eric@molitor.org>
2020-05-09 13:43:32 +0100

    Add ifplugd

--- ifplugd.c
+++ ifplugd.c
@@ -29,165 +29,172 @@ config IFPLUGD
 #include <fnmatch.h>
 #include <linux/if_tunnel.h>
 
-#ifndef IP_DF
-#define IP_DF 0x4000  /* don't fragment flag. */
-#endif
+//#ifndef IP_DF
+//#define IP_DF 0x4000  /* don't fragment flag. */
+//#endif
+
+#define BUF_SIZE 8192
+#define DEFAULT_ACTION "/etc/ifplugd/ifplugd.action"
 
 GLOBALS(
   int unused;
 )
 
-// little helper to parsing message using netlink macroses
-void parseRtattr(struct rtattr *tb[], int max, struct rtattr *rta, int len)
-{
-  memset(tb, 0, sizeof(struct rtattr *) * (max + 1));
-
-  while (RTA_OK(rta, len)) {  // while not end of the message
-    if (rta->rta_type <= max) {
-      tb[rta->rta_type] = rta; // read attr
+void create_socket(int32_t *sd, struct sockaddr_nl *nl) {
+    *sd = socket(AF_NETLINK, SOCK_DGRAM, NETLINK_ROUTE);
+    if (*sd == -1) {
+        error_exit("Socket error");
     }
-    rta = RTA_NEXT(rta,len);    // get next attr
-  }
+
+    if ( bind(*sd, (struct sockaddr *) nl, sizeof( struct sockaddr_nl)) == -1) {
+        error_exit("Bind error");
+    }
+}
+
+void run(char **command) {
+    pid_t pid = fork();
+
+    if (pid == -1) {
+        error_exit("Fork failed");
+    } else if (pid == 0) {
+        // we are the child
+        execvp(command[0], command);
+        error_exit("%s error", command[0]);
+    }
+}
+
+ssize_t send_message(int32_t *sd, struct nlmsghdr *hdr, struct sockaddr_nl *nl) {
+    struct iovec *iov = xmalloc(sizeof(struct iovec));
+    iov->iov_base = hdr;
+    iov->iov_len = hdr->nlmsg_len;
+
+    struct msghdr *msg = xmalloc(sizeof(struct msghdr));;
+    msg->msg_name = nl;
+    msg->msg_namelen = sizeof(struct sockaddr_nl);
+    msg->msg_iov = iov;
+    msg->msg_iovlen = 1;
+
+    ssize_t msg_len = sendmsg(*sd, msg, 0);
+    if (msg_len < 0) {
+        error_exit("netlink send failed: %s", strerror(errno));
+    }
+
+    free(iov);
+    free(msg);
+
+    return msg_len;
+}
+
+ssize_t receive_message(int32_t *sd, char *buf, struct sockaddr_nl *nl) {
+    struct iovec *iov = xmalloc(sizeof(struct iovec));
+    iov->iov_base = buf;
+    iov->iov_len = sizeof(char[BUF_SIZE]);
+
+    struct msghdr *msg = xmalloc(sizeof(struct msghdr));
+    msg->msg_name = nl;
+    msg->msg_namelen = sizeof(struct sockaddr_nl);
+    msg->msg_iov = iov;
+    msg->msg_iovlen = 1;
+
+    ssize_t msg_len = recvmsg(*sd, msg, 0);
+    if (msg_len < 0) {
+        error_exit(stderr, "netlink read failed: %s", strerror(errno));
+    }
+
+    free(iov);
+    free(msg);
+
+    return msg_len;
+}
+
+void init_interfaces(void) {
+    struct nlmsghdr hdr;
+    struct sockaddr_nl nl;
+    char buf[BUF_SIZE];
+    int32_t sd;
+
+    memset(&nl, 0, sizeof(nl));
+    nl.nl_family = AF_NETLINK;
+
+    create_socket(&sd, &nl);
+
+    memset(&hdr, 0, sizeof(hdr));
+    hdr.nlmsg_len = NLMSG_LENGTH(sizeof(struct rtgenmsg));
+    hdr.nlmsg_type = RTM_GETLINK;
+    hdr.nlmsg_flags = NLM_F_REQUEST | NLM_F_DUMP;
+    hdr.nlmsg_pid = (__u32) getpid();
+    hdr.nlmsg_seq = 1;
+
+    send_message(&sd, &hdr, &nl);
+
+    ssize_t msg_len = receive_message(&sd, buf, &nl);
+
+    struct nlmsghdr *nlmsg = (struct nlmsghdr *) buf;
+    while (NLMSG_OK(nlmsg, msg_len)) {
+        if (nlmsg->nlmsg_type != RTM_NEWLINK) continue;
+
+        struct ifinfomsg *ifi = NLMSG_DATA(nlmsg);
+        struct rtattr *attr = IFLA_RTA(ifi);
+        uint32_t attr_len = nlmsg->nlmsg_len - NLMSG_LENGTH(sizeof(*ifi));
+
+        while (RTA_OK(attr, attr_len)) {
+            if (attr->rta_type == IFLA_IFNAME) {
+                char *ifName = (char *) RTA_DATA(attr);
+                run((char *[]) {DEFAULT_ACTION, ifName, "init", 0});
+            }
+            attr = RTA_NEXT(attr, attr_len);
+        }
+        nlmsg = NLMSG_NEXT(nlmsg, msg_len);
+    }
+
+    close(sd);
+}
+
+
+void ifplugd(void) {
+    struct sockaddr_nl nl;
+    char buf[BUF_SIZE];
+    int32_t sd;
+
+    memset(&nl, 0, sizeof(nl));
+    nl.nl_family = AF_NETLINK;
+    nl.nl_groups = RTMGRP_LINK;
+
+    create_socket(&sd, &nl);
+
+    while (1) {
+        ssize_t msg_len = receive_message(&sd, buf, &nl);
+
+        struct nlmsghdr *nlmsg = (struct nlmsghdr *) buf;
+        while (NLMSG_OK(nlmsg, msg_len)) {
+            if (nlmsg->nlmsg_type != RTM_NEWLINK) continue;
+
+            struct ifinfomsg *ifi = NLMSG_DATA(nlmsg);
+            struct rtattr *attr = IFLA_RTA(ifi);
+            uint32_t attr_len = nlmsg->nlmsg_len - NLMSG_LENGTH(sizeof(*ifi));
+
+            while (RTA_OK(attr, attr_len)) {
+                if (attr->rta_type == IFLA_IFNAME) {
+                    char *ifName = (char *) RTA_DATA(attr);
+                    if (ifi->ifi_flags & IFF_RUNNING) { // get UP flag of the network interface
+                        run((char *[]) {DEFAULT_ACTION, ifName, "up", 0});
+                    } else {
+                        run((char *[]) {DEFAULT_ACTION, ifName, "down", 0});
+                    }
+                }
+                attr = RTA_NEXT(attr, attr_len);
+            }
+
+            nlmsg = NLMSG_NEXT(nlmsg, msg_len);
+        }
+    }
+
+    close(sd);
 }
 
 void ifplugd_main(void)
 {
-    int fd = socket(AF_NETLINK, SOCK_RAW, NETLINK_ROUTE);   // create netlink socket
-
-    if (fd < 0) {
-        error_exit("Failed to create netlink socket: %s\n", (char*)strerror(errno));
-    }
-
-    struct sockaddr_nl  local;  // local addr struct
-    char buf[8192];             // message buffer
-    struct iovec iov;           // message structure
-    iov.iov_base = buf;         // set message buffer as io
-    iov.iov_len = sizeof(buf);  // set size
-
-    memset(&local, 0, sizeof(local));
-
-    local.nl_family = AF_NETLINK;       // set protocol family
-    local.nl_groups =   RTMGRP_LINK | RTMGRP_IPV4_IFADDR | RTMGRP_IPV4_ROUTE;   // set groups we interested in
-    local.nl_pid = getpid();    // set out id using current process id
-
-    // initialize protocol message header
-    struct msghdr msg;  
-    {
-        msg.msg_name = &local;                  // local address
-        msg.msg_namelen = sizeof(local);        // address size
-        msg.msg_iov = &iov;                     // io vector
-        msg.msg_iovlen = 1;                     // io size
-    }   
-
-    if (bind(fd, (struct sockaddr*)&local, sizeof(local)) < 0) {     // bind socket
-        error_exit("Failed to bind netlink socket: %s\n", (char*)strerror(errno));
-        //close(fd);
-    }   
-
-    // read and parse all messages from the
-    while (1) {
-        ssize_t status = recvmsg(fd, &msg, MSG_DONTWAIT);
-
-        //  check status
-        if (status < 0) {
-            if (errno == EINTR || errno == EAGAIN)
-            {
-                usleep(250000);
-                continue;
-            }
-
-            xprintf("Failed to read netlink: %s", (char*)strerror(errno));
-            continue;
-        }
-
-        if (msg.msg_namelen != sizeof(local)) { // check message length, just in case
-            xprintf("Invalid length of the sender address struct\n");
-            continue;
-        }
-
-        // message parser
-        struct nlmsghdr *h;
-
-        for (h = (struct nlmsghdr*)buf; status >= (ssize_t)sizeof(*h); ) {   // read all messagess headers
-            int len = h->nlmsg_len;
-            int l = len - sizeof(*h);
-            char *ifName;
-
-            if ((l < 0) || (len > status)) {
-                xprintf("Invalid message length: %i\n", len);
-                continue;
-            }
-
-            // now we can check message type
-            if ((h->nlmsg_type == RTM_NEWROUTE) || (h->nlmsg_type == RTM_DELROUTE)) { // some changes in routing table
-                xprintf("Routing table was changed\n");  
-            } else {    // in other case we need to go deeper
-                char *ifUpp;
-                char *ifRunn;
-                struct ifinfomsg *ifi;  // structure for network interface info
-                struct rtattr *tb[IFLA_MAX + 1];
-
-                ifi = (struct ifinfomsg*) NLMSG_DATA(h);    // get information about changed network interface
-
-                parseRtattr(tb, IFLA_MAX, IFLA_RTA(ifi), h->nlmsg_len);  // get attributes
-                
-                if (tb[IFLA_IFNAME]) {  // validation
-                    ifName = (char*)RTA_DATA(tb[IFLA_IFNAME]); // get network interface name
-                }
-
-                if (ifi->ifi_flags & IFF_UP) { // get UP flag of the network interface
-                    ifUpp = (char*)"UP";
-                } else {
-                    ifUpp = (char*)"DOWN";
-                }
-
-                if (ifi->ifi_flags & IFF_RUNNING) { // get RUNNING flag of the network interface
-                    ifRunn = (char*)"RUNNING";
-                } else {
-                    ifRunn = (char*)"NOT RUNNING";
-                }
-
-                char ifAddress[256];    // network addr
-                struct ifaddrmsg *ifa; // structure for network interface data
-                struct rtattr *tba[IFA_MAX+1];
-
-                ifa = (struct ifaddrmsg*)NLMSG_DATA(h); // get data from the network interface
-
-                parseRtattr(tba, IFA_MAX, IFA_RTA(ifa), h->nlmsg_len);
-
-                if (tba[IFA_LOCAL]) {
-                    inet_ntop(AF_INET, RTA_DATA(tba[IFA_LOCAL]), ifAddress, sizeof(ifAddress)); // get IP addr
-                }
-
-                switch (h->nlmsg_type) { // what is actually happenned?
-                    case RTM_DELADDR:
-                        xprintf("Interface %s: address was removed\n", ifName);
-                        break;
-
-                    case RTM_DELLINK:
-                        xprintf("Network interface %s was removed\n", ifName);
-                        break;
-
-                    case RTM_NEWLINK:
-                        xprintf("New network interface %s, state: %s %s\n", ifName, ifUpp, ifRunn);
-                        break;
-
-                    case RTM_NEWADDR:
-                        xprintf("Interface %s: new address was assigned: %s\n", ifName, ifAddress);
-                        break;
-                }
-            }
-
-            status -= NLMSG_ALIGN(len); // align offsets by the message length, this is important
-
-            h = (struct nlmsghdr*)((char*)h + NLMSG_ALIGN(len));    // get next message
-        }
-
-        usleep(250000); // sleep for a while
-    }
-
-  //xprintf("Hello world\n");
-
-  // Avoid kernel panic if run as init.
-  //if (getpid() == 1) wait(&TT.unused);
+    signal(SIGCHLD, SIG_IGN);
+    init_interfaces();
+    ifplugd();
 }
